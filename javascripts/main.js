995d1276a031042fd0d122b816cf83dce226acf7
kheilma <kyleheilman@Kyles-MacBook-Pro.local>
2015-07-28 19:32:18 -0500

    sss

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,9 @@
 console.log('This would be the main JS file.');
+<script>
+function myFunction(e) {
+    x = e.clientX;
+    y = e.clientY;
+    coor = "Coordinates: (" + x + "," + y + ")";
+    document.getElementById("demo").innerHTML = coor
+}
+</script>
