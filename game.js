58a7e67ea4f346971b1adbc8a7c7dafb8546ebc4
kheilma <kheilma@purdue.edu>
2015-12-22 01:26:02 -0600

    dasasd
    dsadas

--- game.js
+++ game.js
@@ -1,46 +1,145 @@
+//------------
+//System Vars
+//------------
 var stage = document.getElementById("gameCanvas");
 stage.width = STAGE_WIDTH;
 stage.height = STAGE_HEIGHT;
 var ctx = stage.getContext("2d");
-ctx.fillStyle = "black";
+ctx.fillStyle = "grey";
 ctx.font = GAME_FONTS;
 
+//---------------
+//Preloading ...
+//---------------
+//Preload Art Assets
+// - Sprite Sheet
 var charImage = new Image();
 charImage.ready = false;
-charImage.onload= setAssetReady;
+charImage.onload = setAssetReady;
 charImage.src = PATH_CHAR;
 
-function setAssetReady() {
+function setAssetReady()
+{
 	this.ready = true;
 }
 
-ctx.fillRect(0,0,stage.width, stage.height);
+//Display Preloading
+ctx.fillRect(0,0,stage.width,stage.height);
 ctx.fillStyle = "#000";
 ctx.fillText(TEXT_PRELOADING, TEXT_PRELOADING_X, TEXT_PRELOADING_Y);
 var preloader = setInterval(preloading, TIME_PER_FRAME);
-var gameloop, currX, currY;
+
+var gameloop, facing, currX, currY, charX, charY, isMoving;
 
 function preloading()
-{
-	if (charImage.ready){
+{	
+	if (charImage.ready)
+	{
 		clearInterval(preloader);
-		gameloop = setInterval(update, TIME_PER_FRAME);
+		
+		//Initialise game
+		facing = "E"; //N = North, E = East, S = South, W = West
+		isMoving = false;
+		
+		gameloop = setInterval(update, TIME_PER_FRAME);			
+		document.addEventListener("keydown",keyDownHandler, false);	
+		document.addEventListener("keyup",keyUpHandler, false);	
 	}
 }
 
-var gameloop = setInterval(update, TIME_PER_FRAME);
-var counter = 0;
+//------------
+//Key Handlers
+//------------
+function keyDownHandler(event)
+{
+	var keyPressed = String.fromCharCode(event.keyCode);
 
+	if (keyPressed == "W")
+	{		
+		facing = "N";
+		isMoving = true;
+	}
+	else if (keyPressed == "D")
+	{	
+		facing = "E";
+		isMoving = true;		
+	}
+	else if (keyPressed == "S")
+	{	
+		facing = "S";
+		isMoving = true;		
+	}
+	else if (keyPressed == "A")
+	{	
+		facing = "W";
+		isMoving = true;		
+	}
+}
+
+function keyUpHandler(event)
+{
+	var keyPressed = String.fromCharCode(event.keyCode);
+	
+	if ((keyPressed == "W") || (keyPressed == "A") || 
+		(keyPressed == "S") || (keyPressed == "D"))
+	{
+		isMoving = false;
+	}
+}
+
+//------------
+//Game Loop
+//------------
+charX = CHAR_START_X;
+charY = CHAR_START_Y;
+
+currX = IMAGE_START_X;
+currY = IMAGE_START_EAST_Y;
 
 function update()
-{
+{		
+	//Clear Canvas
 	ctx.fillStyle = "grey";
-	ctx.fillRect(0,0, stage.width, stage.height);
-
-	ctx.drawImage(charImage, currX, currY, CHAR_WIDTH, CHAR_HEIGHT, CHAR_START_X, CHAR_START_Y, CHAR_WIDTH, CHAR_HEIGHT);
-
-	currX += CHAR_WIDTH;
-	if (currX >= SPRITE_WIDTH){
+	ctx.fillRect(0, 0, stage.width, stage.height);	
+	
+	if (isMoving)
+	{
+		if (facing == "N")
+		{
+			charY -= CHAR_SPEED;
+			currY = IMAGE_START_NORTH_Y;
+		}
+		else if (facing == "E")
+		{
+			charX += CHAR_SPEED;
+			currY = IMAGE_START_EAST_Y;
+		}
+		else if (facing == "S")
+		{
+			charY += CHAR_SPEED;
+			currY = IMAGE_START_SOUTH_Y;
+		}
+		else if (facing == "W")
+		{
+			charX -= CHAR_SPEED;
+			currY = IMAGE_START_WEST_Y;
+		}
+		
+		currX += CHAR_WIDTH;
+		
+		if (currX >= SPRITE_WIDTH)
 			currX = 0;
 	}
+	
+	//Draw Image
+	ctx.drawImage(charImage,currX,currY,CHAR_WIDTH,CHAR_HEIGHT,
+					charX,charY,CHAR_WIDTH,CHAR_HEIGHT);
+
 }
+
+
+
+
+
+	
+	
